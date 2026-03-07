08126e29fc0ed43080dffe2cbdf9e1324e4df08e
Ryn-coding <aryanvaghela53@gmail.com>
2026-03-07 13:05:42 +0530

    Restaurant Menu

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,18 @@
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $28.99', 'Pasta - $18.50', 'Burger - $15.99', 'Salmon - $26.99'];
+const dessertMenu = ['Cake - $8.99', 'Ice Cream - $6.50', 'Pudding - $5.99', 'Fruit Salad - $7.99'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;
+});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;
+}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
